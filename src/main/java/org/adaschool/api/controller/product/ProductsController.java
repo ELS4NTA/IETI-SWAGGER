61c73cf48f174540a24620f47451a68ee81e6bd8
Daniel Santanilla <daniel1sa_@hotmail.com>
2024-02-06 14:45:20 -0500

    API REST con Spring Boot - Microservicios Lab

--- src/main/java/org/adaschool/api/controller/product/ProductsController.java
+++ src/main/java/org/adaschool/api/controller/product/ProductsController.java
@@ -1,13 +1,23 @@
 package org.adaschool.api.controller.product;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.adaschool.api.exception.ProductNotFoundException;
 import org.adaschool.api.repository.product.Product;
+import org.adaschool.api.repository.product.ProductDto;
 import org.adaschool.api.service.product.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/products/")
@@ -20,34 +30,46 @@ public class ProductsController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct() {
-        //TODO implement this method
-        URI createdProductUri = URI.create("");
-        return ResponseEntity.created(createdProductUri).body(null);
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
+        Product newProduct = productsService.save(new Product(productDto));
+        URI createdProductUri = URI.create("/v1/products/" + newProduct.getId());
+        return ResponseEntity.created(createdProductUri).body(newProduct);
     }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+        List<Product> products = productsService.all();
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Product> findById(@PathVariable("id") String id) {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+        Optional<Product> product = productsService.findById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        }
+        throw new ProductNotFoundException(id);
     }
 
-    @PutMapping
-    public ResponseEntity<Product> updateProduct() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    @PutMapping("{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") String id, @RequestBody ProductDto productDto) {
+        Optional<Product> findedProduct = productsService.findById(id);
+        if (findedProduct.isPresent()) {
+            Product updatedProduct = findedProduct.get();
+            updatedProduct.update(productDto);
+            Product newProduct = productsService.save(updatedProduct);
+            return ResponseEntity.ok(newProduct);
+        }
+        throw new ProductNotFoundException(id);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteProduct() {
-        //TODO implement this method
-
-        return ResponseEntity.ok().build();
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") String id) {
+        Optional<Product> product = productsService.findById(id);
+        if (product.isPresent()) {
+            productsService.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        throw new ProductNotFoundException(id);
     }
 }
--- src/main/java/org/adaschool/api/service/product/ProductsServiceMap.java
+++ src/main/java/org/adaschool/api/service/product/ProductsServiceMap.java
@@ -3,33 +3,40 @@ package org.adaschool.api.service.product;
 import org.adaschool.api.repository.product.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class ProductsServiceMap implements ProductsService {
+
+    Map<String, Product> products = new HashMap<>();
+
     @Override
     public Product save(Product product) {
-        return null;
+        products.put(product.getId(), product);
+        return product;
     }
 
     @Override
     public Optional<Product> findById(String id) {
-        return Optional.empty();
+        return Optional.ofNullable(products.get(id));
     }
 
     @Override
     public List<Product> all() {
-        return null;
+        return (List<Product>) products.values();
     }
 
     @Override
     public void deleteById(String id) {
-
+        products.remove(id);
     }
 
     @Override
     public Product update(Product product, String productId) {
-        return null;
+        products.put(productId, product);
+        return product;
     }
 }
